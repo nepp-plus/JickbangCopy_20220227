3a9482e1d899eba988c8b2da9796b0f27cb89bdf
nepp.plus <nepp.plus@gmail.com>
2022-02-27 14:36:34 +0900

    방 목록데이터 추가 (임시로 직접 타이핑)

--- app/src/main/java/com/neppplus/jickbangcopy_20220227/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/MainActivity.kt
@@ -2,10 +2,26 @@ package com.neppplus.jickbangcopy_20220227
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20220227.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRoomList.add( RoomData(8000, "서울시 동대문구", 7, "1번째 방입니다.") )
+        mRoomList.add( RoomData(15000, "서울시 동대문구", 1, "2번째 방입니다.") )
+        mRoomList.add( RoomData(27000, "서울시 동대문구", -2, "3번째 방입니다.") )
+        mRoomList.add( RoomData(6500, "서울시 서대문구", 0, "4번째 방입니다.") )
+        mRoomList.add( RoomData(3800, "서울시 서대문구", 8, "5번째 방입니다.") )
+        mRoomList.add( RoomData(79000, "서울시 서대문구", 0, "6번째 방입니다.") )
+        mRoomList.add( RoomData(16500, "서울시 강남구", -1, "7번째 방입니다.") )
+        mRoomList.add( RoomData(21500, "서울시 강남구", 6, "8번째 방입니다.") )
+        mRoomList.add( RoomData(153000, "서울시 강북구", 12, "9번째 방입니다.") )
+        mRoomList.add( RoomData(125000, "서울시 강북구", 8, "10번째 방입니다.") )
+
     }
 }
