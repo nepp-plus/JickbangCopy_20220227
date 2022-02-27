1afee4b1557ae98c56c6f7afce91c29820f4fbd5
nepp.plus <nepp.plus@gmail.com>
2022-02-27 15:00:59 +0900

    방의 층수값에 따라, 다른 형태로 가공하는 함수 추가 제작

--- app/src/main/java/com/neppplus/jickbangcopy_20220227/datas/RoomData.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/datas/RoomData.kt
@@ -9,7 +9,25 @@ class RoomData(
     val description: String,
 ) {
 
-//    층수값 => 1층, 반지하, 지하 1층 형태로 가공
+//    층수값 => "1층", "반지하", "지하 1층" 형태로 가공
+
+    fun getFormattedFloor() : String {
+
+        val floorStr : String
+
+        if (floor > 0) {
+            floorStr = "${floor}층"
+        }
+        else if (floor == 0) {
+            floorStr = "반지하"
+        }
+        else {
+            floorStr = "지하 ${ -floor }층"
+        }
+
+        return floorStr
+
+    }
     
     
 //    가격값 => "8,500",  "2억 3,500" 형태로 가공
