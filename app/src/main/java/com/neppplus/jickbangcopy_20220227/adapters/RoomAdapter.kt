31b5b8e48270baa2677b9ea9b0edc411cf12b8e1
nepp.plus <nepp.plus@gmail.com>
2022-02-27 14:05:50 +0900

    어댑터 클래스 추가, ArrayAdapter<RoomData> 상속   - 기본 생성자를 지원 안함.

---
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/adapters/RoomAdapter.kt
@@ -0,0 +1,7 @@
+package com.neppplus.jickbangcopy_20220227.adapters
+
+import android.widget.ArrayAdapter
+import com.neppplus.jickbangcopy_20220227.datas.RoomData
+
+class RoomAdapter : ArrayAdapter<RoomData>() {
+}
