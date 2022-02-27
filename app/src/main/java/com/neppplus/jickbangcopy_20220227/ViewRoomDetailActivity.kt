695f330f2e24b2b8dfcfddd7ac6b92bfb75a6d62
nepp.plus <nepp.plus@gmail.com>
2022-02-27 16:09:16 +0900

    받아온 데이터를 이용, 실제 UI에 반영

--- app/src/main/java/com/neppplus/jickbangcopy_20220227/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.jickbangcopy_20220227
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jickbangcopy_20220227.datas.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
 
@@ -11,6 +12,12 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_room_detail)
 
         val roomData =  intent.getSerializableExtra("room") as RoomData
+
+        txtPrice.text =  roomData.getFormattedPrice()
+        txtDescription.text = roomData.description
+
+        txtAddress.text = roomData.address
+        txtFloor.text = roomData.getFormattedFloor()
 
 
     }
