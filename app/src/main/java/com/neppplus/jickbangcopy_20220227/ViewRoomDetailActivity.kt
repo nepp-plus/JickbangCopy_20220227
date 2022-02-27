9c060ea487283b43a7bf62efff66ec174f31cb58
nepp.plus <nepp.plus@gmail.com>
2022-02-27 15:59:40 +0900

    Serializable로 putExtra 할 수 있도록, RoomData 수정

--- app/src/main/java/com/neppplus/jickbangcopy_20220227/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/ViewRoomDetailActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ViewRoomDetailActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+
     }
+
 }
