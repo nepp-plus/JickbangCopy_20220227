64b744b7146bc66c2d2cf7cb4b11902a1c38353d
nepp.plus <nepp.plus@gmail.com>
2022-02-27 14:02:11 +0900

    방 데이터 표현용 클래스 생성  - 생성자를 수정, 4개의 멤버변수 설정

---
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/datas/RoomData.kt
@@ -0,0 +1,13 @@
+package com.neppplus.jickbangcopy_20220227.datas
+
+// 하나의 방 -> 가격, 행정구역, 층수, 설명문구 멤버변수로.
+
+class RoomData(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val description: String,
+) {
+
+
+}
