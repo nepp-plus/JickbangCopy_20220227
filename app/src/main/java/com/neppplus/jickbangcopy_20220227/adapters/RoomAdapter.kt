3692d2e57f9f09da1777a3a353741d90209d534f
nepp.plus <nepp.plus@gmail.com>
2022-02-27 14:08:44 +0900

    어댑터 클래스의 생성자 수정, 부모클래스의 생성자에 변수들 전달

--- app/src/main/java/com/neppplus/jickbangcopy_20220227/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/adapters/RoomAdapter.kt
@@ -1,7 +1,12 @@
 package com.neppplus.jickbangcopy_20220227.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.neppplus.jickbangcopy_20220227.datas.RoomData
 
-class RoomAdapter : ArrayAdapter<RoomData>() {
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<RoomData>
+) : ArrayAdapter<RoomData>(mContext, resId, mList) {
 }
