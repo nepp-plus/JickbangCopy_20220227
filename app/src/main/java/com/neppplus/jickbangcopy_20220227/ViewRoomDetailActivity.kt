940f43affcee1e5747779176f650580b7f1dcb36
nepp.plus <nepp.plus@gmail.com>
2022-02-27 16:03:08 +0900

    보내준 클래스를 받아서 사용.  - as RoomData로 원상복구해서 사용

--- app/src/main/java/com/neppplus/jickbangcopy_20220227/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/ViewRoomDetailActivity.kt
@@ -2,12 +2,15 @@ package com.neppplus.jickbangcopy_20220227
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20220227.datas.RoomData
 
 class ViewRoomDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val roomData =  intent.getSerializableExtra("room") as RoomData
 
 
     }
